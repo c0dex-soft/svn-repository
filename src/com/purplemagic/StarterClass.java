8103b408f23c73fed9b8f941f42d907d2ff72c56
purple-magic <markop.018@gmail.com>
2017-11-13 15:33:32 +0100

    Commit 04

--- src/com/purplemagic/StarterClass.java
+++ src/com/purplemagic/StarterClass.java
@@ -5,5 +5,6 @@ public class StarterClass {
     public static void main(String[] args) {
         System.out.println("Starting SVN demo.");
         System.out.println("First commit done successfully");
+        System.out.println("First commit agains");
     }
 }
